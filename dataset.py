02fab86d1a5dc0ad4bd8384beff1aadafebbd24b
Jin Park <jpark96@berkeley.edu>
2019-05-17 16:43:04 -0700

    Implemented new SiameseDataset() for modularizing the LandmarksDataset from sampling for siamese learning.

--- dataset.py
+++ dataset.py
@@ -120,24 +120,54 @@ class SiameseLandmarksDataset(Dataset):
                   'label': torch.from_numpy(np.array(int(landmark_id0 != landmark_id1))).float()}
         return sample
 
-if __name__ == "__main__":
-#    landmark_dataset = LandmarksDataset(csv_file='small-dataset/small-dataset.csv',
-#                                        root_dir='small-dataset/',
-#                                        transform=transforms.Compose([
-#                                                  transforms.ToPILImage(),
-#                                                  transforms.Resize(256),
-#                                                  transforms.RandomCrop(244),
-#                                                  transforms.ToTensor()]))
-#    print("Dataset size: " + str(len(landmark_dataset)))
-#    print("Row 0: " + str(landmark_dataset[0]))
+# Load data
+class SiameseDataset(Dataset):
+    """Landmarks dataset."""
 
-    siamese_landmark_dataset = SiameseLandmarksDataset(csv_file='toy-dataset/toy-dataset.csv',
-                                    root_dir='toy-dataset/',
-                                    transform=transforms.Compose([
-                                               transforms.ToPILImage(),
-                                               transforms.Resize(256),
-                                               transforms.RandomCrop(244),
-                                               transforms.ToTensor()]))
+    def __init__(self, dataset):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.dataset = dataset
+        
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        # Get two items, with 50% chance similarity/dissimilarity
+        landmark_id0, landmark_id1 = None, None
+        should_be_similar = np.random.randint(2)
+        while True:
+            idx0, idx1 = np.random.choice(len(self.dataset), size=2)
+
+            landmark_id0 = self.dataset[idx0]['label']
+            landmark_id1 = self.dataset[idx1]['label']
+
+            if (should_be_similar and (landmark_id0 == landmark_id1)): break
+            if (not should_be_similar and (landmark_id0 != landmark_id1)): break
+ 
+        # Return sample
+        sample = {'image0': self.dataset[idx0]['image'], 'image1': self.dataset[idx1]['image'],
+                  'label': torch.from_numpy(np.array(int(landmark_id0 != landmark_id1))).float()}
+        return sample
+
+if __name__ == "__main__":
+    landmark_dataset = LandmarksDataset(csv_file='small-dataset/small-dataset.csv',
+                                       root_dir='small-dataset/',
+                                       transform=transforms.Compose([
+                                                 transforms.ToPILImage(),
+                                                 transforms.Resize(256),
+                                                 transforms.RandomCrop(244),
+                                                 transforms.ToTensor()]))
+    print("Dataset size: " + str(len(landmark_dataset)))
+    print("Row 0: " + str(landmark_dataset[0]))
+
+    siamese_landmark_dataset = SiameseDataset(dataset=landmark_dataset)
+
     sample = next(iter(siamese_landmark_dataset))
     image0, image1, label = sample['image0'], sample['image1'], sample['label']
     plt.imshow(image0.transpose(0, 2).transpose(0, 1))
