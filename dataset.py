838f237c4dd3edad67da2e5b9d2654a688c00180
Jin Park <jpark96@berkeley.edu>
2019-05-22 12:55:11 -0700

    deleted SiameseLandmarkDataset

--- dataset.py
+++ dataset.py
@@ -53,74 +53,6 @@ class LandmarksDataset(Dataset):
         return sample
 
 # Load data
-class SiameseLandmarksDataset(Dataset):
-    """Landmarks dataset."""
-
-    def __init__(self, csv_file, root_dir, transform=None):
-        """
-        Args:
-            csv_file (string): Path to the csv file with annotations.
-            root_dir (string): Directory with all the images.
-            transform (callable, optional): Optional transform to be applied
-                on a sample.
-        """
-        self.landmarks_metadata = pd.read_csv(csv_file)
-        self.root_dir = root_dir
-        self.transform = transform
-        
-        # Bidict from ids to labels to keep labels within [0, num_classes]
-        self.id_to_label = dict()
-        self.label_to_id = dict()
-        self.num_classes = 0
-        
-
-    def __len__(self):
-        return len(self.landmarks_metadata)
-
-    def __getitem__(self, idx):
-        # Get two items, with 50% chance similarity/dissimilarity
-        landmark_id0, landmark_id1 = None, None
-        should_be_similar = np.random.randint(2)
-        while True:
-            idx0, idx1 = np.random.choice(len(self.landmarks_metadata), size=2)
-
-            landmark_id0 = self.landmarks_metadata['landmark_id'][idx0]
-            landmark_id1 = self.landmarks_metadata['landmark_id'][idx1]
-
-            if (should_be_similar and (landmark_id0 == landmark_id1)): break
-            if (not should_be_similar and (landmark_id0 != landmark_id1)): break
-        
-        # Get images
-        id0 = self.landmarks_metadata['id'][idx0]
-        img_name0 = self.root_dir + str(landmark_id0) + "/" + str(id0) + ".jpg"
-        image0 = io.imread(img_name0)
-        
-        id1 = self.landmarks_metadata['id'][idx1]
-        img_name1 = self.root_dir + str(landmark_id1) + "/" + str(id1) + ".jpg"
-        image1 = io.imread(img_name1)
-        
-        # If id is not seen, add to id2label bidict
-        if landmark_id0 not in self.id_to_label:
-            self.id_to_label[landmark_id0] = self.num_classes
-            self.label_to_id[self.num_classes] = landmark_id0
-            self.num_classes += 1
-            
-        if landmark_id1 not in self.id_to_label:
-            self.id_to_label[landmark_id1] = self.num_classes
-            self.label_to_id[self.num_classes] = landmark_id1
-            self.num_classes += 1
-        
-        # Transform images
-        if self.transform:
-            image0 = self.transform(image0)
-            image1 = self.transform(image1)
- 
-        # Return sample
-        sample = {'image0': image0, 'image1': image1,
-                  'label': torch.from_numpy(np.array(int(landmark_id0 != landmark_id1))).float()}
-        return sample
-
-# Load data
 class SiameseDataset(Dataset):
     """Landmarks dataset."""
 
@@ -141,7 +73,7 @@ class SiameseDataset(Dataset):
         # Get two items, with 50% chance similarity/dissimilarity
         landmark_id0, landmark_id1 = None, None
         should_be_similar = np.random.randint(2)
-        while True:
+        for i in range(10):
             idx0, idx1 = np.random.choice(len(self.dataset), size=2)
 
             landmark_id0 = self.dataset[idx0]['label']
